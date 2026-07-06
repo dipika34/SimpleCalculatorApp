b1522f8c8758e739851b93f02329eef5e42d0c2b
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-07-06 20:53:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function display(num){
+     document.getElementById("demo").value +=num;
+   
+}
+function Calculate(){
+    document.getElementById("demo").value
+    = eval(document.getElementById("demo").value);
+    
+}
